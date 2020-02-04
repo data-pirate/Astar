d9a8f0f20495e735b3104b52858115b822cbda32
Damanpreet Singh <43677641+data-pirate@users.noreply.github.com>
2020-02-04 19:52:41 +0530

    update

--- shopping_site/shop/urls.py
+++ shopping_site/shop/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blog/', views.blog, name='blog')
+    path('blog/', views.blog, name='blog'),
+    path('cart/', views.cart, name='cart'),
+    path('categories/', views.categories, name='category'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('product/', views.product, name='Products')
 ]
--- shopping_site/shop/views.py
+++ shopping_site/shop/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def blog(request):
     return render(request, 'blog.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def product(request):
+    return render(request, 'product.html')
+
+def categories(request):
+    return render(request, 'categories.html')
