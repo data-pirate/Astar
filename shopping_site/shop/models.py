edd313dd976dcac9bdd1d0b9215b8e38f0429be5
Damanpreet Singh <43677641+data-pirate@users.noreply.github.com>
2020-02-06 18:29:22 +0530

    updated models

--- shopping_site/shop/models.py
+++ shopping_site/shop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=150)
+    category = models.CharField(max_length=100, default='')
+    sub_category = models.CharField(max_length=100, default='')
+    price = models.FloatField(default=0)
+    desc = models.CharField(max_lentgh=200, defafult='')
+    date = models.DateField()
+    image = models.ImageField(upload_to='shop/product_images', default='')
+
+
+    def __str__(self):
+        return self.product_name
